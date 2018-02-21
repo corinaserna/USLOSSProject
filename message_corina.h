b8817de8a20abb161ffe37c560b1de3f8fde2845
Cayile Dawson <cnd@email.arizona.edu>
2018-02-21 10:25:55 -0700

    Update message_corina.h

--- message_corina.h
+++ message_corina.h
@@ -1,21 +1,47 @@
 #define DEBUG2 1
 
 typedef struct Mailbox      Mailbox;
+typedef struct Mailbox      *MailboxPtr;
 typedef struct Mailslot     Mailslot;
+typedef struct Mailslot    *MailslotPtr;
+typedef struct Queue      Queue;
+typedef struct Queue      *QueuePtr;
+typedef struct QueueEl      QueueEl;
+typedef struct QueueEl      *QueueElPtr;
+
+struct Queue {
+    QueueElPtr first;
+    QueueElPtr last;
+    int count;
+};
+
+struct QueueEl {
+    QueueElPtr prev;
+    QueueElPtr next;
+    void *val;
+    int val2;
+};
+
+QueueElPtr popQueue(QueuePtr thisQueue);
+void pushQueue(QueuePtr thisQueue, QueueElPtr val);
 
 struct Mailbox {
     int         mboxID;
-    int         totalMailSlots;
-    int         activeMailSlots;
+    int         index;
+   int          totalMailSlots;
     int         maxMessageSize;
-    Mailslot    *mailSlotTable;
+    QueueElPtr  mailBoxQ_El;           // queue element for this mailbox
+    Queue		blockedPID_Q;
+    Queue       mailSlot_Q;
 };
 
 struct Mailslot {
     int         mboxID;
-    int         mailSlotTableIndex; // I dont think I need this
-    int         totalSlotTableIndex;
+//    int         mailSlotTableIndex; // I dont think I need this
+//    int         totalSlotTableIndex;
     // int         status;
+    QueueElPtr  mailSlotQ_El;           // queue element for this mailSlot
+    int         maxMsgSize;     // don't free messages unless need to increase size
     int         msgSize;
     void        *msg;
 };
